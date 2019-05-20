2004ed332fff396b4668248aaea1e07616d72227
kazu <kazu8160@gmail.com>
2019-05-20 22:59:56 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/highhi/gig
+
+require (
+	github.com/Netflix/go-expect v0.0.0-20180928190340-9d1f4485533b // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/hinshun/vt10x v0.0.0-20180809195222-d55458df857c // indirect
+	github.com/kr/pty v1.1.4 // indirect
+	github.com/mattn/go-colorable v0.1.1 // indirect
+	github.com/mattn/go-isatty v0.0.7 // indirect
+	github.com/stretchr/testify v1.3.0 // indirect
+	github.com/urfave/cli v1.20.0
+	golang.org/x/crypto v0.0.0-20190513172903-22d7a77e9e5f // indirect
+	golang.org/x/sys v0.0.0-20190516110030-61b9204099cb // indirect
+	gopkg.in/AlecAivazis/survey.v1 v1.8.4
+)
---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+
+	"github.com/urfave/cli"
+	"gopkg.in/AlecAivazis/survey.v1"
+)
+
+const tempPath = "./templates"
+
+var tempMap = map[string]map[string]string{
+	"gitignore": {
+		"input":  "gitignore.txt",
+		"output": ".gitignore",
+	},
+	"npmignore": {
+		"input":  "npmignore.txt",
+		"output": ".npmignore",
+	},
+}
+
+func makeFile(cwd string, input string, output string) {
+	temp, err := ioutil.ReadFile(path.Join(tempPath, input))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile(path.Join(cwd, output), temp, 0644)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func makeFiles(fileNames []string) {
+	cwd, _ := os.Getwd()
+
+	for _, value := range fileNames {
+		temp := tempMap[value]
+		makeFile(cwd, temp["input"], temp["output"])
+	}
+}
+
+func keys(m map[string]string) []string {
+	ks := []string{}
+
+	for k := range m {
+		ks = append(ks, k)
+	}
+
+	return ks
+}
+
+func appAction(c *cli.Context) error {
+	tempFiles := []string{}
+	keys := []string{}
+
+	for key := range tempMap {
+		keys = append(keys, key)
+	}
+
+	prompt := &survey.MultiSelect{
+		Message: "Please select the required file",
+		Options: keys,
+	}
+
+	err := survey.AskOne(prompt, &tempFiles, nil)
+
+	if err != nil {
+		return err
+	}
+
+	makeFiles(tempFiles)
+
+	return nil
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "gig"
+
+	app.Action = appAction
+
+	err := app.Run(os.Args)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
